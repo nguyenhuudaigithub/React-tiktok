d953dccad7a9190bef2be6e930d50b379e00e603
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-04-10 23:40:03 +0700

    Mounted & Unmounted

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
 import { useState } from "react";
 
+import Content from "./Content";
+
 function App() {
-  const [job, setJob] = useState("");
-  const [lists, setLists] = useState(() => {
-    const storageLists = JSON.parse(localStorage.getItem("lists"));
-    return storageLists ?? [];
-  });
-
-  const handleAdd = () => {
-    setLists((prev) => {
-      const newLists = [...prev, job];
-
-      // Save to local storage
-      const jsonLists = JSON.stringify(newLists);
-
-      localStorage.setItem("lists", jsonLists);
-
-      return newLists;
-    });
-    setJob("");
-  };
-
-  const handleDelete = (index) => {
-    const newLists = lists.filter((list, i) => i !== index);
-    localStorage.setItem("lists", JSON.stringify(newLists));
-    setLists(newLists);
-  };
+  const [show, setShow] = useState(false);
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Job List</h1>
-      <input
-        value={job}
-        onChange={(e) => {
-          setJob(e.target.value);
-        }}
-      />
-      <button style={{ marginLeft: 10 }} onClick={handleAdd}>
-        Add
-      </button>
-      <ul>
-        {lists.map((list, index) => (
-          <li key={index}>
-            {list}
-            <button
-              style={{ marginLeft: 10 }}
-              onClick={() => handleDelete(index)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div style={{ padding: 32 }}>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+
+      {show && <Content />}
     </div>
   );
 }
