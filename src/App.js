5d854f3b43160540cc483d42e805a5fee46ad96a
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-04-10 23:11:17 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [counter,setCouter] = useState(1);
+  
+  const handleIncrease = () => {
+    setCouter(counter+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1> {counter}</h1>
+       <button onClick={handleIncrease}> +1 </button>
     </div>
   );
 }
