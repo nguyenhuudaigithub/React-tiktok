cad42d61841eb2ffb1e6f81e0c26675f33a2ecfa
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-04-10 23:39:22 +0700

    useState - two-way

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import { useState } from "react";
+
 function App() {
-  const [counter,setCouter] = useState(1);
-  
-  const handleIncrease = () => {
-    setCouter(counter+1)
-  }
+  const [job, setJob] = useState("");
+  const [lists, setLists] = useState(() => {
+    const storageLists = JSON.parse(localStorage.getItem("lists"));
+    return storageLists ?? [];
+  });
+
+  const handleAdd = () => {
+    setLists((prev) => {
+      const newLists = [...prev, job];
+
+      // Save to local storage
+      const jsonLists = JSON.stringify(newLists);
+
+      localStorage.setItem("lists", jsonLists);
+
+      return newLists;
+    });
+    setJob("");
+  };
+
+  const handleDelete = (index) => {
+    const newLists = lists.filter((list, i) => i !== index);
+    localStorage.setItem("lists", JSON.stringify(newLists));
+    setLists(newLists);
+  };
 
   return (
-    <div className="App">
-       <h1> {counter}</h1>
-       <button onClick={handleIncrease}> +1 </button>
+    <div style={{ padding: 20 }}>
+      <h1>Job List</h1>
+      <input
+        value={job}
+        onChange={(e) => {
+          setJob(e.target.value);
+        }}
+      />
+      <button style={{ marginLeft: 10 }} onClick={handleAdd}>
+        Add
+      </button>
+      <ul>
+        {lists.map((list, index) => (
+          <li key={index}>
+            {list}
+            <button
+              style={{ marginLeft: 10 }}
+              onClick={() => handleDelete(index)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
