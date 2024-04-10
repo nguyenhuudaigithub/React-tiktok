70217dd5ea1726c19c64bd13c5fc8e7f979cb206
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-04-11 00:04:34 +0700

    update-Dom and call api

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// 1.useEffect(callback)
+//  - Gọi callback mỗi khi component re-render
+//  - Gọi callback sau khi component thêm element vào DOM
+// 2.useEffect(callback,[])
+// 3.useEffect(callback,[deps])
+//===============================
+// 1.callback luôn được gọi sau khi component mount
+//
 
 function Content() {
-    const [show, setShow] = useState(false);
-  
-    return (
-      <div style={{ padding: 32 }}>
-       <h1> Hello</h1>
-      </div>
-    );
-  }
-  
-  export default Content;
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    document.title = title;
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts));
+  }, []);
+
+  return (
+    <div>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Content;
